590198d310746f68a3ee6d2a301de74b790bfa28
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-24 14:22:58 +0100

    Update implementation for baby steps

--- src/main/java/com/codurance/LeapYear.java
+++ src/main/java/com/codurance/LeapYear.java
@@ -2,7 +2,7 @@ package com.codurance;
 
 public class LeapYear {
     public boolean isLeapYear(int year) {
-        if(year % 4 == 0)
+        if(year == 1996)
             return true;
         return false;
     }
