383278cf3c845bdee00df0f34ed1103c3f4c7fbe
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-24 14:58:41 +0100

    Pass test for year 1700

--- src/main/java/com/codurance/LeapYear.java
+++ src/main/java/com/codurance/LeapYear.java
@@ -2,7 +2,7 @@ package com.codurance;
 
 public class LeapYear {
     public boolean isLeapYear(int year) {
-        if(year == 1800)
+        if(year % 100 == 0 && year % 400 != 0)
             return false;
         if(isDivisibleBy4(year))
             return true;
